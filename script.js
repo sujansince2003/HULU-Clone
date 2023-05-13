af55991f9635f03f7635ca9fcc6f8aded8dfee8e
SUJAN KHATRI <99068384+sujansince2003@users.noreply.github.com>
2023-05-13 22:41:27 +0545

    Update script.js
    update for action in clicking hamburger menu

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
-// let hamburger=document.querySelector(".hamburger");
-let modal=document.querySelector(".login-modal-container");
-let closemodal=document.querySelector(".closemodal");
-let openmodal=document.querySelector(".openmodal");
-openmodal.addEventListener("click",showmodal);
-closemodal.addEventListener("click",closemodal);
-
+let openmodal=document.querySelector(".openmodal")
+let closemodal=document.querySelector(".closemodal")
+let loginmodal=document.querySelector(".login-modal-container");
+closemodal.style.display="none";
+openmodal.addEventListener("click",showmodal)
+closemodal.addEventListener("click",closemodall)
 
 function showmodal()
 {
-     modal.style.display="inline";
-     closemodal.style.display="inline";
-     openmodal.style.display="none"
-        
-
-
-        
-   
+          loginmodal.style.display="inline";
+          openmodal.style.display="none";
+          closemodal.style.display="inline";
 }
-function closemodal()
+
+function closemodall()
 {
-    
-  
-     openmodal.style.display="inline"
-        }
+          loginmodal.style.display="none";
+          openmodal.style.display="inline";
+          closemodal.style.display="none";
+}
